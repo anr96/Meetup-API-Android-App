38cb7064b69fc929043d482d3dce54b8d7c2e00f
Walter Galan <mr2incredible@gmail.ccom>
2016-12-10 18:01:52 -0800

    Successfully added Location. Obtains Latitude and Longitude in MeetUpListFragment.

--- EventBrowserFinale/app/src/main/java/com/fall16/csc413/team12/eventbrowserfinale/MeetUpListFragment.java
+++ EventBrowserFinale/app/src/main/java/com/fall16/csc413/team12/eventbrowserfinale/MeetUpListFragment.java
@@ -30,10 +30,16 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
 
+import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -41,84 +47,53 @@ import java.util.List;
  */
 
 public class MeetUpListFragment extends Fragment implements SearchView.OnQueryTextListener,
-		GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks {
+		GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks, LocationListener {
 
-    private RecyclerView mStoryRecyclerView;
-    private StoryAdapter mAdapter;
-	//JsonController controller;
+	private static final String TAG = "MeetUpListFragment";
 
-	/**
-	 * Provides the entry point to Google Play services.
-	 */
+	// The desired interval for location updates. Inexact. Updates may be more or less frequent.
+	public static final long UPDATE_INTERVAL_IN_MILLISECONDS = 10000;
+
+	// The fastest rate for active location updates. Exact
+	public static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS =
+			UPDATE_INTERVAL_IN_MILLISECONDS / 2;
+
+	// Keys for storing activity state in the Bundle.
+	protected final static String REQUESTING_LOCATION_UPDATES_KEY =
+			"requesting-location-updates-key";
+	protected final static String LOCATION_KEY = "location-key";
+	protected final static String LAST_UPDATED_TIME_STRING_KEY =
+			"last-updated-time-string-key";
+
+	// Provides the entry point to Google Play services.
 	private GoogleApiClient mGoogleApiClient;
 
-	/**
-	 * Represents a geographical location.
-	 */
-	private Location mLastLocation;
+	// Stores parameters for requests to the FusedLocationProviderApi.
+	private LocationRequest mLocationRequest;
+
+	// Represents a geographical location.
+	protected Location mCurrentLocation;
+
 	private double mLatitude;
 	private double mLongitude;
 
-	private static final String TAG = "MeetUpListFragment";
+	// Tracks the status of the location updates request.
+	protected Boolean mRequestingLocationUpdates = true;
+
+	// Time when the location was updated represented as a String.
+	protected String mLastUpdateTime;
 
 	// int required to ask permission for location
 	private static final int MY_PERMISSION_ACCESS_COARSE_LOCATION = 11;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setHasOptionsMenu(true);
-
-		// Create an instance of GoogleAPIClient.
-		buildGoogleApiClient();
-	}
-
-	@Override
-<<<<<<< HEAD:EventBrowserFinale/app/src/main/java/com/fall16/csc413/team12/eventbrowserfinale/StoryListFragment.java
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-							 Bundle savedInstanceState){
-		View view = inflater.inflate(R.layout.fragment_story_list, container, false);
-=======
-	public void onConnected(Bundle connectionHint) {
-
-		if (ContextCompat.checkSelfPermission(App.getContext(),
-				android.Manifest.permission.ACCESS_COARSE_LOCATION )
-				!= PackageManager.PERMISSION_GRANTED ) {
-
-			ActivityCompat.requestPermissions(getActivity(), new String[] {
-					android.Manifest.permission.ACCESS_COARSE_LOCATION },
-					MY_PERMISSION_ACCESS_COARSE_LOCATION);
-		}
-
-		mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
-				mGoogleApiClient);
-		if (mLastLocation != null) {
-			mLatitude = mLastLocation.getLatitude();
-			Log.i(TAG, "Latitude is: " + mLatitude);
-			mLongitude = mLastLocation.getLongitude();
-			Log.i(TAG, "Longitude is: " + mLongitude);
-
-			//mLatitudeTextView.setText(String.valueOf(mLastLocation.getLatitude()));
-			//mLongitudeTextView.setText(String.valueOf(mLastLocation.getLongitude()));
-		}
-	}
-
-	@Override
-	public void onConnectionSuspended(int i) {
-		Log.i(TAG, "Connection Suspended");
-		mGoogleApiClient.connect();
-	}
-
-	@Override
-	public void onConnectionFailed(ConnectionResult connectionResult) {
-		Log.i(TAG, "Connection failed. Error: " + connectionResult.getErrorCode());
-	}
+	private RecyclerView mStoryRecyclerView;
+	private StoryAdapter mAdapter;
+	//JsonController controller;
 
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.fragment_meet_up_list, container, false);
->>>>>>> origin:EventBrowserFinale/app/src/main/java/com/fall16/csc413/team12/eventbrowserfinale/MeetUpListFragment.java
 
 		// Create the toolbar for this fragment
 		Toolbar toolbar = (Toolbar) view.findViewById(R.id.my_toolbar);
@@ -131,10 +106,19 @@ public class MeetUpListFragment extends Fragment implements SearchView.OnQueryTe
 		mStoryRecyclerView = (RecyclerView) view.findViewById(R.id.story_recycler_view);
 		mStoryRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
-		updateUI();
+		mRequestingLocationUpdates = false;
+		mLastUpdateTime = "";
+
+		// Update values using data stored in the Bundle.
+		updateValuesFromBundle(savedInstanceState);
+
+		// Create an instance of GoogleAPIClient.
+		buildGoogleApiClient();
 
 		// Required for SearchView implementation
-		//setHasOptionsMenu(true);
+		setHasOptionsMenu(true);
+
+		updateUI();
 
 		/*
 		controller = new JsonController(
@@ -237,25 +221,77 @@ public class MeetUpListFragment extends Fragment implements SearchView.OnQueryTe
 		return true;
 	}
 
-	/**
-	 * Builds a GoogleApiClient. Uses the addApi() method to request the LocationServices API.
-	 */
-	protected synchronized void buildGoogleApiClient() {
+	private void updateValuesFromBundle(Bundle savedInstanceState) {
+		Log.i(TAG, "Updating values from bundle");
+		if (savedInstanceState != null) {
+			// Update the value of mRequestingLocationUpdates from the Bundle, and make sure that
+			// the Start Updates and Stop Updates buttons are correctly enabled or disabled.
+			if (savedInstanceState.keySet().contains(REQUESTING_LOCATION_UPDATES_KEY)) {
+				mRequestingLocationUpdates = savedInstanceState.getBoolean(
+						REQUESTING_LOCATION_UPDATES_KEY);
+			}
 
-		if (mGoogleApiClient == null) {
-			mGoogleApiClient = new GoogleApiClient.Builder(App.getContext())
-					.addConnectionCallbacks(this)
-					.addOnConnectionFailedListener(this)
-					.addApi(LocationServices.API)
-					.build();
+			// Update the value of mCurrentLocation from the Bundle and update the UI to show the
+			// correct latitude and longitude.
+			if (savedInstanceState.keySet().contains(LOCATION_KEY)) {
+				// Since LOCATION_KEY was found in the Bundle, we can be sure that mCurrentLocation
+				// is not null.
+				mCurrentLocation = savedInstanceState.getParcelable(LOCATION_KEY);
+			}
+
+			// Update the value of mLastUpdateTime from the Bundle and update the UI.
+			if (savedInstanceState.keySet().contains(LAST_UPDATED_TIME_STRING_KEY)) {
+				mLastUpdateTime = savedInstanceState.getString(LAST_UPDATED_TIME_STRING_KEY);
+			}
 		}
 	}
 
+	// Builds a GoogleApiClient. Uses the addApi() method to request the LocationServices API.
+	protected synchronized void buildGoogleApiClient() {
+
+		Log.i(TAG, "Building GoogleApiClient");
+
+		mGoogleApiClient = new GoogleApiClient.Builder(App.getContext())
+				.addConnectionCallbacks(this)
+				.addOnConnectionFailedListener(this)
+				.addApi(LocationServices.API)
+				.build();
+
+		createLocationRequest();
+	}
+
 	protected void createLocationRequest() {
-		LocationRequest mLocationRequest = new LocationRequest();
-		mLocationRequest.setInterval(10000);
-		mLocationRequest.setFastestInterval(5000);
+
+		mLocationRequest = new LocationRequest();
+		mLocationRequest.setInterval(UPDATE_INTERVAL_IN_MILLISECONDS);
+		mLocationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS);
 		mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+	}
+
+	// Requests location updates from the FusedLocationApi.
+	protected void startLocationUpdates() {
+
+		// Check for location permissions
+		if (ContextCompat.checkSelfPermission(App.getContext(),
+				android.Manifest.permission.ACCESS_COARSE_LOCATION )
+				!= PackageManager.PERMISSION_GRANTED ) {
+
+			ActivityCompat.requestPermissions(getActivity(), new String[] {
+							android.Manifest.permission.ACCESS_COARSE_LOCATION },
+					MY_PERMISSION_ACCESS_COARSE_LOCATION);
+		}
+
+		LocationServices.FusedLocationApi.requestLocationUpdates(
+				mGoogleApiClient, mLocationRequest, this);
+	}
+
+	// Removes location updates from the FusedLocationApi.
+	protected void stopLocationUpdates() {
+		// It is a good practice to remove location requests when the activity is in a paused or
+		// stopped state. Doing so helps battery performance and is especially
+		// recommended in applications that request frequent location updates.
+
+		LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
 	}
 
 	@Override
@@ -273,10 +309,8 @@ public class MeetUpListFragment extends Fragment implements SearchView.OnQueryTe
 
 	@Override
 	public void onConnected(Bundle connectionHint) {
-		// Provides a simple way of getting a device's location and is well suited for
-		// applications that do not require a fine-grained location and that do not need location
-		// updates. Gets the best and most recent location currently available, which may be null
-		// in rare cases when a location is not available.
+
+		Log.i(TAG, "Connected to GoogleApiClient");
 
 		// Check for location permissions
 		if (ContextCompat.checkSelfPermission(App.getContext(),
@@ -288,16 +322,18 @@ public class MeetUpListFragment extends Fragment implements SearchView.OnQueryTe
 					MY_PERMISSION_ACCESS_COARSE_LOCATION);
 		}
 
-		getLocation();
+		if (mCurrentLocation == null) {
+			mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+			mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
 
-		if (mLastLocation != null) {
-			mLatitude = mLastLocation.getLatitude();
+			mLatitude = mCurrentLocation.getLatitude();
 			Log.i(TAG, "Latitude is: " + mLatitude);
-			mLongitude = mLastLocation.getLongitude();
+			mLongitude = mCurrentLocation.getLongitude();
 			Log.i(TAG, "Longitude is: " + mLongitude);
 		}
-		else {
-			Toast.makeText(getActivity(), R.string.no_location_detected, Toast.LENGTH_LONG).show();
+
+		if (mRequestingLocationUpdates) {
+			startLocationUpdates();
 		}
 	}
 
@@ -312,6 +348,25 @@ public class MeetUpListFragment extends Fragment implements SearchView.OnQueryTe
 		Log.i(TAG, "Connection failed. Error: " + connectionResult.getErrorCode());
 	}
 
+	@Override
+	public void onLocationChanged(Location location) {
+		mCurrentLocation = location;
+		mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+
+		mLatitude = mCurrentLocation.getLatitude();
+		Log.i(TAG, "Location Changed. New Latitude is: " + mLatitude);
+		mLongitude = mCurrentLocation.getLongitude();
+		Log.i(TAG, "Location Changed. New Longitude is: " + mLongitude);
+	}
+
+	// Stores activity data in the Bundle.
+	public void onSaveInstanceState(Bundle savedInstanceState) {
+		savedInstanceState.putBoolean(REQUESTING_LOCATION_UPDATES_KEY, mRequestingLocationUpdates);
+		savedInstanceState.putParcelable(LOCATION_KEY, mCurrentLocation);
+		savedInstanceState.putString(LAST_UPDATED_TIME_STRING_KEY, mLastUpdateTime);
+		super.onSaveInstanceState(savedInstanceState);
+	}
+
     // ViewHolder holds onto a View
     private class StoryHolder extends RecyclerView.ViewHolder
             implements View.OnClickListener{
@@ -322,20 +377,17 @@ public class MeetUpListFragment extends Fragment implements SearchView.OnQueryTe
         private ImageView mImageView;
         private Story mStory;
 
+        public StoryHolder(View itemView) {
 
-
-        public StoryHolder(View itemView){
             super(itemView);
             itemView.setOnClickListener(this);
 
             mNameTextView = (TextView) itemView.findViewById(R.id.list_item_card_name);
             mStoryNameTextView = (TextView) itemView.findViewById(R.id.list_item_story_name);
 
-            mDescriptionTextView = (TextView) itemView.findViewById(R.id.list_item_story_description);
+            mDescriptionTextView =
+					(TextView) itemView.findViewById(R.id.list_item_story_description);
             mImageView = (ImageView) itemView.findViewById(R.id.list_item_story_card);
-
-
-
         }
 
         
